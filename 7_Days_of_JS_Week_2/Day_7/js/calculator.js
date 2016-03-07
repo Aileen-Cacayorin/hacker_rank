c9882186c6d8f2bf865e0ff392dc46c5fe51e3bb
Aileen <aileencacayorin@gmail.com>
2016-03-07 13:45:33 -0800

    refactor and add comments to js

--- 7_Days_of_JS_Week_2/Day_7/js/calculator.js
+++ 7_Days_of_JS_Week_2/Day_7/js/calculator.js
@@ -1,38 +1,22 @@
 function calculator() {
   var action = function() {
-                var e = window.event;
-                var btn = e.target || e.srcElement;
+                var e = window.event; // looks for event in window, assigns var e to that event
+                var btn = e.target || e.srcElement; // gets the clicked element - srcElement for IE, target for others
 
-                var value = document.getElementById(btn.id).innerHTML;
-                var results = document.getElementById("res");
-                if (value == "C") {
+                var value = document.getElementById(btn.id).innerHTML; //gets value of btn
+                var results = document.getElementById("res"); //gets value of res container
+                if (value == "C") { //clears res container if btnClr is clicked
                   results.innerHTML = "";
+                } else if (value == "=") { //runs calculation if btnEql is clicked
+                    var new_value = getOperandsAndOperator()
+                    results.innerHTML = new_value
 
-                } else if (value == "=") {
-                    var values = getOperandsAndOperator();
-                    var final;
-                    try {
-                      if (values[0] == "+") {
-                        final = add(values[1], values[2]);
-
-                      } else if (values[0] == "-") {
-                        final = sub(values[1], values[2]);
-
-                      } else if (values[0] == "*") {
-                        final = multiply(values[1], values[2])
-
-                      } else if (values[0] == "/") {
-                        final = divide(values[1], values[2])
-                      };
-
-                    } finally {
-                        results.innerHTML = final;
-                    };
-                } else {
+                } else { //adds number to res container
                     var resultsValue = results.innerHTML;
                     results.innerHTML = resultsValue + value;
                 };
             };
+  // adds click event listner to btn and calls action() function whenever btn is clicked
   document.getElementById("btn0").addEventListener("click", action);
   document.getElementById("btn1").addEventListener("click", action);
   document.getElementById("btnClr").addEventListener("click", action);
@@ -44,32 +28,10 @@ function calculator() {
 }
 
 function getOperandsAndOperator() {
-  var resContents = (document.getElementById("res")).innerHTML.trim();
-  var operator = (resContents.match(/[\/\+\-\*]/))[0];
-  var operands = resContents.split(operator);
-  var operand1 = parseInt(operands[0], 2);
-  var operand2 = parseInt(operands[1], 2);
-  return [operator, operand1, operand2]
-
-}
-
-function add(value1, value2) {
-  var sum = parseInt(value1+value2, 10).toString(2);
-  return sum;
-}
-
-function sub(value1, value2) {
-  var diff= parseInt(value1-value2, 10).toString(2);
-  return diff;
-
-}
-
-function multiply(value1, value2) {
-  var product = parseInt(value1*value2, 10).toString(2);
-  return product;
-}
-
-function divide(value1, value2) {
-  var quotient = parseInt(value1/value2, 10).toString(2);
-  return quotient;
+  var resContents = (document.getElementById("res")).innerHTML.trim(); // gets res contents as string and trims whitespaces
+  var operator = (resContents.match(/[\/\+\-\*]/))[0]; //gets operator as string
+  var operands = resContents.split(operator); //splits res contents along the operator
+  var operand1 = parseInt(operands[0], 2); //converts operand1 from binary to base 10
+  var operand2 = parseInt(operands[1], 2);// converts operand2 from binary to base 10
+  return eval(operand1 + operator + operand2).toString(2) // calculates and then converts back to binary
 }
